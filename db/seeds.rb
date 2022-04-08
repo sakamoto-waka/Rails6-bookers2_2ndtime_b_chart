f33e38c8be5d0d2a5832d28a9df892191037c3b1
sakamoto-waka <wakanmhl@gmail.com>
2022-04-08 22:40:51 +0000

    [update] seeds success to add users+books dummy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 User.create!(name: 'aaaa',
-             email: 'aaaa@gmail.com',
-             password: 'aaaaaa')
+            email: 'aaaa@gmail.com',
+            password: 'aaaaaa')
 
 10.times do |n|
-  name: Faker::Name.name
-  email: 'example-#{n+1}@gmail.com'
-  password: 'password'
+  name = Faker::Name.name
+  email = "example-#{n+1}@gmail.com"
+  password = 'password'
   User.create!(name: name,
-               email: email,
-               password: password)
+              email: email,
+              password: password)
 end
 
 # books
@@ -16,5 +16,6 @@ users = User.order(:created_at).take(6)
 3.times do
   title = Faker::Book.title
   body = Faker::Lorem.sentence(word_count: 5)
+  
   users.each { |user| user.books.create!(title: title, body: body) }
-  end
+end
